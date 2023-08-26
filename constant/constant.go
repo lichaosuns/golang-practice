c2be684e048dbf77b5f561711c82fca6ba11476b
lichaosuns <1741721630@qq.com>
2023-08-26 16:52:15 +0800

    [feat]: go constant
    [body]: go constant

---
+++ constant/constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const constant_1 = 10
+const (
+	constant_2 = "hello"
+	constant_3 = "world"
+)
+
+func main() {
+	fmt.Println(constant_1)
+	fmt.Println(constant_3)
+}
