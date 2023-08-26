78859f7f8796435840ea780c338d3ca558c0a257
lichaosuns <1741721630@qq.com>
2023-08-26 17:20:39 +0800

    [feat]: string to int
    [body]: string to int

---
+++ type_convertion/string_to_integer.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+func main() {
+	intStr := "100"
+
+	// 自动识别
+	intVal, err := strconv.Atoi(intStr)
+	fmt.Println(intVal, err, reflect.TypeOf(intVal))
+
+	intStr = "1aa"
+	// 自动识别
+	intVal, err = strconv.Atoi(intStr)
+	fmt.Println(intVal, err, reflect.TypeOf(err), reflect.TypeOf(intVal))
+
+	// 转换
+
+	intStr2 := "0b1000"
+
+	int64Val, err := strconv.ParseInt(intStr2, 0, 8)
+
+	fmt.Println(int64Val, err, reflect.TypeOf(int64Val))
+
+}
